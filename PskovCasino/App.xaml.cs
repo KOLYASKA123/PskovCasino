57ce7f504b9af708555962f1a9f87f3a3cda3c2d
Ivanov Nikolai <n.kolka.r60@gmail.com>
2024-05-26 18:59:42 +0300

    Добавьте файлы проекта.

---
+++ PskovCasino/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PskovCasino.MVVM.ViewModel;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PskovCasino
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly ServiceProvider _serviceProvider;
+
+        public App()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddSingleton<MainWindow>(provider => new MainWindow
+            {
+                DataContext = provider.GetRequiredService<MainViewModel>()
+            });
+            services.AddSingleton<MainViewModel>();
+            services.AddSingleton<RegistrationViewModel>();
+            services.AddSingleton<LoginViewModel>();
+
+
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+            base.OnStartup(e);
+        }
+    }
+
+}
